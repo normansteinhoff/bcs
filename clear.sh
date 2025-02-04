d7b8cbb6316cfd4d2cf3ab8f6ba15ae275a9bf05
norman steinhoff <normansteinhoff@hotmail.de>
2025-02-04 16:04:28 +0100

    .

---
+++ clear.sh
@@ -0,0 +1,40 @@
+
+if [ -f .*aux ];then
+    rm *.aux
+fi
+
+if [ -f .*bbl ];then
+    rm *.bbl
+fi
+
+if [ -f .*blg ];then
+    rm *.blg
+fi
+
+if [ -f .*fdb_latexmk ];then
+    rm *.fdb_latexmk
+fi
+
+if [ -f .*fls ];then
+    rm *.fls
+fi
+
+if [ -f .*log ];then
+    rm *.log
+fi
+
+if [ -f .*gz ];then
+    rm *.gz
+fi
+
+if [ -f .*toc ];then
+    rm *.toc
+fi
+
+if [ -f .*out ];then
+    rm *.out
+fi
+
+if [ -f .*pdf ];then
+    rm *.pdf
+fi
